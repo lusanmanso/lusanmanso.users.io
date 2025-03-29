28a69fb5037f0639f5b9fd13df4d41bfae6a96f0
Lucía <lucia.manso@live.u-tad.com>
2025-03-29 20:07:18 +0100

    feat(email): create transporter

--- utils/handleEmail.js
+++ utils/handleEmail.js
@@ -1,4 +1,40 @@
 // File: utils/handleEmail.js
 const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
+const OAuth2 = google.auth.OAuth2;
 
-// Configurar transporte de correo
+/**
+ * Create an OAuth2 authenticated transporter for sending emails
+ * @returns {Promise<nodemailer.Transporter>} Configured nodemailer transporter
+ */
+const createTransporter = async () => {
+    const oauth2Client = new OAuth2(
+        process.env.CLIENT_ID,
+        process.env.CLIENT_SECRET,
+        process.env.REDIRECT_URI,
+    );
+    oauth2Client.setCredentials({refresh_token: process.env.REFRESH_TOKEN});
+
+    const accessToken = await new Promise((resolve, reject) => {
+        oauth2Client.getAccessToken((err, token) => {
+            if (err) {
+                reject('Error retrieving access token');
+            }
+            resolve(token);
+        });
+    });
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            type: 'OAuth2',
+            user: process.env.EMAIL,
+            accessToken,
+            clientId: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            refreshToken: process.env.REFRESH_TOKEN,
+        }
+    });
+
+    return transporter;
+};
