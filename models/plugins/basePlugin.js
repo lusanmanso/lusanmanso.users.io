d08173b060fc023ba92f1a4eab9084c323a8f23b
Lucía <lusanmanso@gmail.com>
2025-06-26 18:22:00 +0200

    feat(plugin): add instance methods for soft-delete, restore, and verification in Mongoose base plugin

--- models/plugins/basePlugin.js
+++ models/plugins/basePlugin.js
@@ -67,6 +67,72 @@
          });
       }
 
+      // Instance methods
+      /**
+       * Archives the document (soft-delete)
+       * @param {Object} userId - Id of the user that archieves the document
+       * @return {Promise<Document>}
+       */
+      schema.methods.softDelete = function(userId) {
+         this.archived = true;
+         this.archivedAt = new Date();
+         if(userId) {
+            this.archivedBy = userId;
+         }
+         return this.save();
+      };
 
+      /**
+       * Restore an archived document
+       * @param {Object} userId - Id of the user that restores the document
+       * returns {Promise<Document>}
+       */
+      schema.methods.restore = function(userId) {
+         this.archived = false;
+         this.archivedAt = null;
+         if(this.archivedBy) {
+            this.archivedBy = undefined;
+         }
+         if (userId) {
+            this.restoredBy = userId;
+            this.restoredAt = new Date();
+         }
+         return this.save();
+      };
+
+      /**
+       * Verify if the document belongs to the user
+       * @param {Object|String} userId - Id of the user to verify
+       * @returns {Boolean}
+       */
+      schema.methods.belongsToUser = function(userId) {
+         return this.createdBy.toString === userId.toString();
+      };
+
+      /**
+       * Verify if the document belongs to the company
+       * @param {Object|String} companyId - Id of the company to verify
+       * @return {Boolean}
+       */
+       schema.methods.belongsToCompany = function(companyId) {
+         return this.company.toString() === companyId.toString();
+       }
+
+       /**
+        * Verify if the document is active (not archived)
+        * @return {Boolean}
+        */
+       schema.methods.isActive = function() {
+         return !this.archived;
+       };
+
+       /**
+        * Verify if the document is archived
+        */
+       schema.methods.isArchived = function() {
+         return this.archived;
+       };
+
+       
 
    }
